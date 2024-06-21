a882ab342ea69a340f94d15339e8fb8af6b77bb7
Om Aryan <aryan2003k@gmail.com>
2024-06-21 11:33:06 +0530

    cleaner response fix

--- Loco/serializers.py
+++ Loco/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from .models import Locomotive
+from collections import OrderedDict
 
 class LocomotivesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Locomotive
-        fields = ['motive_power','gauge','name', 'traction', 'usage', 'series', 'numbers', 'img_src', 'manufacturer','axles','numbers_built', 'production','power', 'status']
-    
+        fields = '__all__'
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result if result[key] != "NA"])
     def electric_filter(LocomotiveSerializer):
         LocomotiveSerializer.Meta.fields = ['motive_power','gauge','name', 'traction', 'usage', 'series', 'img_src', 'manufacturer','axles','numbers_built', 'production','power', 'status']
     def broadDiesel_filter(LocomotiveSerializer):
